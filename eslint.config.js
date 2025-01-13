75a0f86095ab41cf8f186d619c39f9c80049ddfb
Takahito Urai <uraitakahito@gmail.com>
2025-01-13 14:24:06 +0900

    update ESLint configuration to support additional file types and customize rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,46 @@
-import js from "@eslint/js";
+import globals from 'globals'
+import js from '@eslint/js';
 
 export default [
   js.configs.all,
   {
-    files: ["**/*.js"],
+    files: ['**/*.js', '**/*.mjs', '**/*.cjs'],
     languageOptions: {
+      globals: {
+        ...globals.node
+      },
       parserOptions: {
-        ecmaVersion: "latest",
+        ecmaVersion: 'latest',
       },
     },
+    rules: {
+      // https://eslint.org/docs/latest/rules/capitalized-comments
+      'capitalized-comments': 'off',
+
+      // https://eslint.org/docs/latest/rules/id-length
+      'id-length': ['error', { 'min': 1 }],
+
+      // https://eslint.org/docs/latest/rules/no-inline-comments
+      'no-inline-comments': 'off',
+
+      // https://eslint.org/docs/latest/rules/no-magic-numbers
+      'no-magic-numbers': [
+        'error', {
+          'detectObjects': false,
+          'enforceConst': true,
+          'ignore': [1],
+          'ignoreArrayIndexes': true,
+          'ignoreClassFieldInitialValues': true,
+        }
+      ],
+      // 'no-magic-numbers': ['error', { 'ignore': [1] }],
+
+      // https://eslint.org/docs/latest/rules/one-var
+      'one-var': ['error', 'never'],
+
+      // https://github.com/airbnb/javascript?tab=readme-ov-file#strings--quotes
+      // https://eslint.org/docs/latest/rules/quotes
+      'quotes': ['error', 'single']
+    }
   },
 ];
